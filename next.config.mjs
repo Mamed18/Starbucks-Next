0bfd602658a6d7ab96d58737efc6bc8d817813a1
Mamed <130478658+Mamed18@users.noreply.github.com>
2024-06-10 14:26:01 +0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// next.config.mjs
+const config = {
+    images: {
+        domains: [
+            'globalassets.starbucks.com',
+            'api.sbux.retter.io',
+            'd2eiylesx4iyph.cloudfront.net',
+            // Add more domains as needed
+        ],
+    },
+};
+
+export default config;
